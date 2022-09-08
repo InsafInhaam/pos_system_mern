fb132668e55e7e4d4537a769dcb265f75ca02756
MohamedAkeem7 <80168526+MohamedAkeem7@users.noreply.github.com>
2022-09-08 17:43:36 +0530

    From Akeem

---
+++ client/src/components/ScrollTop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ScrollTop = () => {
+  return (
+    <>
+    
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    
+    </>
+  )
+}
+
+export default ScrollTop
